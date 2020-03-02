81bd372d53f861c3fc16b4b7aa7b8a33fd2a7c59
Avril Harding <aviharding@gmail.com>
2020-03-02 16:01:56 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+   <div>"
+ end
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "This is Avril's secret page"
+end
+
+set :session_secret, 'super secret'
